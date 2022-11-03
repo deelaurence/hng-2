f4416d1d57d99e188a9ac218e0d16600254b51ef
deelaurence <laurie143pelumi@gmail.com>
2022-11-03 19:16:56 +0100

    cors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors =require ('cors')
 const app = express()
-app.use(cors('*'))
+app.use(cors({origin:"*"}))
 app.use(express.json())
 const port = process.env.PORT||4000
 app.listen(port,()=>{
